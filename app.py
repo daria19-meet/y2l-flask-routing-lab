37e1b108643cdb3b5e142063a5b5a68f412f1211
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:48:20 +0300

    flask

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
+    return render_template("home.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
