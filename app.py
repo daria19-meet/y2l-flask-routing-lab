085465f61581adcbd2e8eafa922d22e66d67626e
aboud20-meet <aboud20@meet.mit.edu>
2018-10-30 16:29:48 +0200

    finalizing flask

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def home_page():
     return render_template("home.html")
 
+@app.route('/shop')
+def shop_page():
+	return render_template("shop.html")
+
+@app.route('/try')
+def try_page():
+	return render_template("try2.html")
+
 if __name__ == '__main__':
    app.run(debug = True)
